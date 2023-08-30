441ebab40b0b3aeeccfbd998fc482a7da6986a7c
newtonehebha <nehebha@mercy.edu>
2023-08-30 15:03:35 -0400

    Initialized properties for enemies and bullets

--- main.py
+++ main.py
@@ -34,3 +34,20 @@ enemy_y_positions = []
 enemy_x_change = []
 enemy_y_change = []
 num_of_enemies = 6
+
+for i in range(num_of_enemies):
+    enemy_img = pygame.image.load("C:\\Users\\newto\\Desktop\\First github pull\\space_arc_redemption\\enemy.png")
+    enemy_img = pygame.transform.scale(enemy_img, (90, 90))  # Resized enemy image
+    enemy_images.append(enemy_img)
+    enemy_x_positions.append(random.randint(0, 835))
+    enemy_y_positions.append(random.randint(50, 150))
+    enemy_x_change.append(4)
+    enemy_y_change.append(40)
+
+bullet_image = pygame.image.load("C:\\Users\\newto\\Desktop\\First github pull\\space_arc_redemption\\redbullet.png")
+bullet_image = pygame.transform.scale(bullet_image, (20, 80))  # Resized bullet image
+bullet_x = 0
+bullet_y = 480
+bullet_x_change = 0
+bullet_y_change = 10
+bullet_state = "ready"
